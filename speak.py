8394519329d60c1729ef437807c6b3312612203a
SonHodor <6164908@gmail.com>
2020-12-22 03:12:29 +0300

    fix some output and cut out voice

--- speak.py
+++ speak.py
@@ -8,38 +8,25 @@ from random import randint
 # pooh gamedev studio montreal (dzr1380)
 # TΔkeshį Øreö ©
 
-#
-# в функцию толк надо запилить вкл выкл звука (глобальная переменная?)
-# сама функция показывает на экран то, что принимает (и говорит)
-#
-def talk(words):
-    print(words)
-    if voice:
-        os.system("say " + words)
 
-
-#
 # функция определяющая уровень сложности
 # возвращает число, между которым будет считаться рандом
-#
 def lvl():
-    talk("Choose. Your. Destiny.")
+    print("Choose. Your. Destiny.")
+    
     difficulty = input("\nВыбери уровень сложности:\n1) Изи пизи\n2) Лемон сквизи\n3) Мазафакер\n")
     if difficulty == "1":
-        num = 20
+        return 20
     elif difficulty == "2":
-        num = 99
+        return 99
     elif difficulty == "3":
-        num = 999
+        return 999
     else:
-        talk("Ты дурачок что-ли?")
-        num = 999999
-    return num
+        print("Ты дурачок что-ли?")
+        return 999999
 
 
-#
 # функция считающая кол-во очков в зависимости от ответа/неответа
-#
 def count_score(mistake, difficulty):  # зададки на норм счетчик
     if mistake:
         if difficulty == 20:
@@ -61,9 +48,7 @@ def count_score(mistake, difficulty):  # зададки на норм счетч
             return 0
 
 
-#
 # функция для сохранения лидерборда в программе (два списка) в порядке убывания
-#
 def ScoreBoard_lists():
     file = open("scoreboard.txt", "r")
     leaders = []
@@ -92,13 +77,11 @@ def ScoreBoard_lists():
     return points, leaders
 
 
-#
 # функция которая делает ДОСКУ ДОСТИЖЕНИЙ
 # 6 ЧАСОВ ДЕЛАЛ АААААА
 # работет на костылях
 # мб когда нибудь сделаю нормально
 # (если смогу)
-#
 def ScoreBoard_insert(score):
     file = open('scoreboard.txt', 'r')
     tmp = ""
@@ -132,13 +115,11 @@ def ScoreBoard_insert(score):
         file.close()
 
 
-#
 # функция чисто показывает на экран доску
-#
 def ScoreBoard_show():
     points, leaders = ScoreBoard_lists()
-    print("##############################################")  # 46 #
-    print("#______________ДОСКА__ПОЧЕТА!________________#")
+    print("################################################")  # 46 #
+    print("#________________ДОСКА__ПОЧЕТА!________________#")
     str = ""
     tmp = 0
     for i in range(0, 10):
@@ -148,94 +129,80 @@ def ScoreBoard_show():
             str += "_"
         str = str + points[i] + " #"
         print(str)
-    print("##############################################")
+    print("################################################")
 
 
-#
 # основная функция которая подразумевает под собой игру
-#
-def game(hardcore):
-    global HP
+def game(hardcore, num):
+    HP = 1
     exit = True
     score = 0
     while exit:
         a = randint(0, num)
         b = randint(0, num)
-        print("Сколько будет ", a, "+", b, "?")
+        print(f"Сколько будет {a} + {b}?")
         try:
             answer = int(input())
             if answer == a + b:
-                talk("Nice")
+                print("Nice")
                 if hardcore:
                     score += count_score(False, num)  # считает очки
-                    print("Score = ", score)
-                    print("Health = ", HP)
+                    print(f"Score = {score}")
+                    print(f"Health = {HP}")
                 else:
                     score += 1  # считает количество ходов
             else:
                 if HP != 0:
-                    talk("Try again")
+                    print("Try again")
                     if hardcore:
                         score += count_score(True, num)  # считает очки
                         HP -= 1
-                        print("Score = ", score)
-                        print("Health = ", HP)
+                        print(f"Score = {score}")
+                        print(f"Health = {HP}")
                     else:
                         score += 1  # считает количество ходов
                 else:
-                    talk("Game over")
+                    print("Game over")
                     exit = False
         except ValueError:
-            talk("Не поняла")
+            print("Не поняла")
     print("Ваш результат: ", score)
     if score <= 0:
-        talk("Неудача")
+        print("Неудача")
     return score
 
 
-'''
-main:
-'''
+def main():
+    print("##############################################")  # 46 #
+    # talk(" Добро пожаловать в SCORE COUNT!")
+    print(" Ver 1.1")
+    print(" Меню:\n 1) Выживание\n 2) Аркада\n 3) Настройки\n 4) Лидерборд")
+    print("##############################################")  # 46 #
+    try:
+        choose = input("Insert coin: ")
+        if choose == "1":
+            # talk("Я пришла сыграть с тобой в игру")
+            # talk("тут должна начаться главная тема из пилы")
+            # talk("сегодня ты будешь у меня считать")
+            # talk("Начнем пожалуй, ты готов?")
+            # talk("Prepare to battle")
+            num = lvl()  # что бы выбрать уровень
+            score = game(True, num)
+            ScoreBoard_insert(score)
+            ScoreBoard_show()
 
-voice = False
-print("##############################################")  # 46 #
-talk(" Добро пожаловать в SCORE COUNT!")
-print(" Ver 1.1")
-print(" Меню:\n 1) Выживание\n 2) Аркада\n 3) Настройки\n 4) Лидерборд")
-print("##############################################")  # 46 #
-try:
-    choose = input("Insert coin: ")
-    if choose == "1":
-        talk("Я пришла сыграть с тобой в игру")
-        talk("тут должна начаться главная тема из пилы")
-        talk("сегодня ты будешь у меня считать")
-        talk("Начнем пожалуй, ты готов?")
-        talk("Prepare to battle")
-        num = lvl()  # что бы выбрать уровень
-        HP = 1
-        score = game(True)
-        ScoreBoard_insert(score)
-        ScoreBoard_show()
+        elif choose == "2":
+            print("Let's go!")
+            num = 99
+            game(False, num)
 
-    elif choose == "2":
-        talk("Let's go!")
-        num = 99
-        game(False)
+        # elif choose == "3":
+        # settings
+        elif choose == "4":
+            ScoreBoard_show()
+    except ValueError:
+        print("Не поняла")
 
-    # elif choose == "3":
-    # settings
-    elif choose == "4":
-        ScoreBoard_show()
-except ValueError:
-    talk("Не поняла")
 
-# чисто что б поболтать:
-'''
-q = False
-
-while not q:
-    words = input("Озвучу: ")
-    Talk(words)
-    if words == "Вырубаюсь":
-        q = True
-'''
+if __name__ == "__main__":
+    main()
